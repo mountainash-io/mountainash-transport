934c5b8defe42bac8300d5db25dfe74177a25c94
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-03-18 15:58:26 +1100

    added tests

---
+++ tests/test_path_utils.py
@@ -0,0 +1,114 @@
+import pytest
+from mountainash_acdrs.utils.path_utils.path_utils import PathUtils
+from mountainash_constants import CONST_STORAGESYSTEM
+
+from upath import UPath
+
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("/path/to/file", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+
+        ("s3://bucket/object", CONST_STORAGESYSTEM.S3.value),
+        # ("gcs://bucket/object", CONST_STORAGESYSTEM.GCS.value),
+        # ("az://container/blob", CONST_STORAGESYSTEM.AZ.value),
+        ("sftp://user@host/path", CONST_STORAGESYSTEM.SFTP.value),
+        ("ssh://user@host/path", CONST_STORAGESYSTEM.SSH.value),
+        ("github://user@host/path", CONST_STORAGESYSTEM.GITHUB.value),
+        ("SSH://user@host/path", CONST_STORAGESYSTEM.SSH.value),
+
+        ("C:\\path\\to\\file", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("D:\\path\\to\\file", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("/", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("~", CONST_STORAGESYSTEM.LOCAL_DISK.value),        
+        ("~/", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+
+        ("~/data/directory/", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("randomfile.txt", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("./randomfile.txt", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("../randomfile.txt", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        ("file:/", CONST_STORAGESYSTEM.LOCAL_DISK.value),
+
+        (UPath("/"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        (UPath("~"), CONST_STORAGESYSTEM.LOCAL_DISK.value),        
+        (UPath("~/"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+
+        (UPath("~/data/directory/"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        (UPath("randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        (UPath("./randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        (UPath("../randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+    ]
+)
+def test_identify_storage_system(path: UPath | str, expected: str):
+    result = PathUtils.identify_storage_system(path)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+
+        ("s3://bucket/object", "s3://bucket/object"),
+        ("s3://bucket/object/", "s3://bucket/object"),
+        # ("github://bucket/object", "github://bucket/object"),
+        # ("nots3butshouldbe://bucket/object", "s3://bucket/object"),
+
+        ("/path/to/file", "/path/to/file"), 
+        ("/path/to/file/", "/path/to/file"), 
+
+
+        ("C:\\path\\to\\file", "C:\\path\\to\\file"),
+        ("D:\\path\\to\\file\\", "D:\\path\\to\\file"),
+
+        ("/", "/"),
+        # ("~", "/Users/nathanielramm"),        
+        # ("~/", "/Users/nathanielramm"),
+
+        ("randomfile.txt", "randomfile.txt"),
+        ("randomfile.txt/", "randomfile.txt"),
+        ("./randomfile.txt", "randomfile.txt"),
+        ("../randomfile.txt", "../randomfile.txt"),
+
+        (UPath("/"), "/"),
+        # (UPath("~"), "/Users/nathanielramm"),        
+        # (UPath("~/"), "/Users/nathanielramm"),
+        # (UPath("/Users/nathanielramm/"), "/Users/nathanielramm"),
+        (UPath("s3://bucket/object"), "s3://bucket/object"),
+        (UPath("s3://bucket/object/"), "s3://bucket/object"),
+
+        # (UPath("~/data/directory/"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        # (UPath("randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        # (UPath("./randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        # (UPath("../randomfile.txt"), CONST_STORAGESYSTEM.LOCAL_DISK.value),
+        # ("s3:bucket/object", "s3://bucket/object"),
+        # ("s3:bucket/object/", "s3://bucket/object"),
+        # ("s3:///bucket/object", "s3://bucket/object"),
+        # ("file:/", "file:/"),
+
+
+    ]
+)
+def test_format_path(path: UPath | str, expected: str):
+
+    result = PathUtils.format_path(path)
+    # result_str = PathUtils.path_to_str(result)
+
+    assert str(result) == str(UPath(expected))
+
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+
+        ("github://bucket/object"),
+        ("nots3butshouldbe://bucket/object"),
+        ("s3:bucket/object"),
+        ("s3:bucket/object/"),
+        ("S3://bucket/object/"),
+    ]
+)
+def test_format_path_invalid(path: UPath | str):
+
+    with pytest.raises(ValueError):
+        PathUtils.format_path(path)
